69507e7a847f0f799fa71ee7d3b7ffb763d9e4ef
dinssa <manny@dinssa.com>
2023-05-18 19:00:41 +0100

    Add ID to database elements

--- db.js
+++ db.js
@@ -24,6 +24,7 @@ const skills = [
 
 for (let i = 0; i < 10; i++) {
     team.push({
+        id: `00${i}`,
         firstname: faker.person.firstName(),
         lastname: faker.person.lastName(),
         gender: faker.person.gender(),
